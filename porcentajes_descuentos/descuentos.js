3b1052b462f9edda0757e48f9d31cea71f4b13f0
eqz.nava@gmail.com <eqz.nava@gmail.com>
2021-11-18 17:12:34 -0600

    Making the responsive for 550px

--- porcentajes_descuentos/descuentos.js
+++ porcentajes_descuentos/descuentos.js
@@ -1,72 +1,67 @@
-// [ok] Generar funcion que haga un descuento
-// [ok] Generar una funcion que cada vez que hagamos click haga el descuento. 
-// Generar funcion que valide el cupon y muestre mensaje de validacion de cupon y retorne el valor del descuento.
-// Generar funcion que aplique el descuento al precio más el valor del cupon. 
-
-function descuentoDePrecio(precio, descuento){
+function descuentoDePrecio(precio, descuento) {
     const porcentajePrecionDescuento = 100 - descuento;
     const precionConDescuento = (precio * porcentajePrecionDescuento) / 100;
 
     return precionConDescuento;
 }
 
-function calcularDescuento(){
+function calcularDescuento() {
     const cupon = document.getElementById('cuponDescuento').value;
     if (cupon === 'cuponA') {
         const precioArticulo = document.getElementById('precioArticulo');
         const priceValue = precioArticulo.value;
         const valorPrecio = parseInt(priceValue);
-    
+
         const porcentajeDescuentoArticulo = document.getElementById('porcentajeDescuentoArticulo');
         const discountValue = porcentajeDescuentoArticulo.value;
         const porcentajeDescuento = parseInt(discountValue);
-        const porcentajeDescuentoFinal = porcentajeDescuento +10;
+        const porcentajeDescuentoFinal = porcentajeDescuento + 10;
         const precioFinal = descuentoDePrecio(valorPrecio, porcentajeDescuentoFinal);
-    
+
         const infoFinal = document.getElementById('infoPrice');
         infoFinal.innerText = `El precio de tu articulo con el ${porcentajeDescuentoFinal}% de descuento es de $${precioFinal}`;
-    }else if(cupon === 'cuponB'){
+    } else if (cupon === 'cuponB') {
         const precioArticulo = document.getElementById('precioArticulo');
         const priceValue = precioArticulo.value;
         const valorPrecio = parseInt(priceValue);
-    
+
         const porcentajeDescuentoArticulo = document.getElementById('porcentajeDescuentoArticulo');
         const discountValue = porcentajeDescuentoArticulo.value;
         const porcentajeDescuento = parseInt(discountValue);
-        const porcentajeDescuentoFinal = porcentajeDescuento +20;
+        const porcentajeDescuentoFinal = porcentajeDescuento + 20;
         const precioFinal = descuentoDePrecio(valorPrecio, porcentajeDescuentoFinal);
-    
+
         const infoFinal = document.getElementById('infoPrice');
         infoFinal.innerText = `El precio de tu articulo con el ${porcentajeDescuentoFinal}% de descuento es de $${precioFinal}`;
-    }else if(cupon === 'cuponC'){
+    } else if (cupon === 'cuponC') {
         const precioArticulo = document.getElementById('precioArticulo');
         const priceValue = precioArticulo.value;
         const valorPrecio = parseInt(priceValue);
-    
+
         const porcentajeDescuentoArticulo = document.getElementById('porcentajeDescuentoArticulo');
         const discountValue = porcentajeDescuentoArticulo.value;
         const porcentajeDescuento = parseInt(discountValue);
-        const porcentajeDescuentoFinal = porcentajeDescuento +30;
+        const porcentajeDescuentoFinal = porcentajeDescuento + 30;
         const precioFinal = descuentoDePrecio(valorPrecio, porcentajeDescuentoFinal);
-    
+
         const infoFinal = document.getElementById('infoPrice');
         infoFinal.innerText = `El precio de tu articulo con el ${porcentajeDescuentoFinal}% de descuento es de $${precioFinal}`;
-    }else{
-    const precioArticulo = document.getElementById('precioArticulo');
-    const priceValue = precioArticulo.value;
-    const valorPrecio = parseInt(priceValue);
+    } else {
+        const precioArticulo = document.getElementById('precioArticulo');
+        const priceValue = precioArticulo.value;
+        const valorPrecio = parseInt(priceValue);
 
-    const porcentajeDescuentoArticulo = document.getElementById('porcentajeDescuentoArticulo');
-    const discountValue = porcentajeDescuentoArticulo.value;
-    const porcentajeDescuento = parseInt(discountValue);
-    const precioFinal = descuentoDePrecio(valorPrecio, porcentajeDescuento);
+        const porcentajeDescuentoArticulo = document.getElementById('porcentajeDescuentoArticulo');
+        const discountValue = porcentajeDescuentoArticulo.value;
+        const porcentajeDescuento = parseInt(discountValue);
+        const precioFinal = descuentoDePrecio(valorPrecio, porcentajeDescuento);
 
-    const infoFinal = document.getElementById('infoPrice');
-    infoFinal.innerText = `El precio de tu articulo con el ${porcentajeDescuento}% de descuento es de $${precioFinal}`;
+        const infoFinal = document.getElementById('infoPrice');
+        infoFinal.innerText = `El precio de tu articulo con el ${porcentajeDescuento}% de descuento es de $${precioFinal}`;
     }
 }
 
-function aplicarCupon(){
+function aplicarCupon() {
     const cupon = document.getElementById('cuponDescuento').value;
     switch (cupon) {
         case 'cuponA':
@@ -86,4 +81,8 @@ function aplicarCupon(){
             infoCupon4.innerText = `El cupon no es válido`;
             break;
     }
+}
+
+function showHelp() {
+    alert('Los cupones disponibles son:\ncuponA\ncuponB\ncuponC');
 }
